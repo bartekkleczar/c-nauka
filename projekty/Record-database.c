488d66b0f653e6eaedfaab16393ad8bcaf831acb
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2023-11-13 19:14:47 +0100

    added removing record and database clear printing functions -DONE

--- projekty/Record-database.c
+++ projekty/Record-database.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct{
+typedef struct {
     int id;
     char content[50];
-}record;
+} record;
 
-void addRecord(record database[100][1], int *databaseCount, record record){
+void addRecord(record database[100][1], int *databaseCount, record record) {
     record.id = *databaseCount;
     database[*databaseCount][0] = record;
     *databaseCount += 1;
 }
 
-void removeRecord(record database[], record record){
-
+void removeRecord(record database[100][1], record toRemove) {
+    strcpy(database[toRemove.id][0].content, "");
+    database[toRemove.id][0].id = (int) -1;
 }
 
-void editRecord(record database[100][1], record record, char *newContent){
+void editRecord(record database[100][1], record record, char *newContent) {
     strcpy(database[record.id][0].content, newContent);
 }
 
-void printDatabase(record database[100][1], int databaseCount){
-    for(int i = 0; i < databaseCount; i++){
+void printDatabase(record database[100][1], int databaseCount) {
+    for (int i = 0; i < databaseCount; i++) {
         printf("\n %d", database[i][0].id);
         printf("\n %s", database[i][0].content);
     }
 }
 
-int main(){
+void printClearDatabase(record database[100][1], int databaseCount) {
+    for (int i = 0; i < databaseCount; i++) {
+        if (database[i][0].id >= 0) {
+            printf("\n %d", database[i][0].id);
+            printf("\n %s", database[i][0].content);
+        }
+    }
+}
+
+int mainRd() {
 
     record database[100][1];
 
@@ -47,6 +57,10 @@ int main(){
 
     printf("\n");
     printDatabase(database, databaseCount);
-    
+
+    removeRecord(database, test0);
+    printf("\n");
+    printClearDatabase(database, databaseCount);
+
     return 0;
 }
